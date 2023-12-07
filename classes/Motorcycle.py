4ce4c11cace9656d300581d16bae4ba1b629e6c6
MatthiasDeGisi <matthias.degisi@gmail.com>
2023-12-06 21:28:00 -0800

    comments and semantics

--- classes/Motorcycle.py
+++ classes/Motorcycle.py
@@ -12,15 +12,41 @@ class Motorcycle(v.Vehicle):
         model: str,
         colour: str,
     ) -> None:
+        """Construct a motorcycle object.
+
+        Args:
+            model (str): motorcycle model
+            colour (str): motorcycle colour
+        """
         super().__init__(model, colour)
         self.__accel = 0.6
 
     def accelerate(self) -> None:
+        """Accelerate the motorcycle based on speed and accel value.
+        """
         self.set_speed(self.get_speed() + self.__accel)
         super().accelerate()
 
     def get_icon(self) -> str:
+        """Get the icon for the motorcycle.
+
+        Returns:
+            str: motorcycle icon
+        """
         return "M"
 
+    def __repr__(self) -> str:
+        """Return a string representation of the motorcycle.
+
+        Returns:
+            str: a string representation of the motorcycle
+        """
+        return "Type: Motorcycle," +  super().__repr__()
+    
     def __str__(self) -> str:
-        return super().__str__()
+        """Return a nicely formatted string with motorcycle info.
+
+        Returns:
+            str: a nicely formatted string with motorcycle info
+        """
+        return super().__str__() + " (Motorcycle)"
--- classes/Vehicle.py
+++ classes/Vehicle.py
@@ -9,6 +9,12 @@ import random
 
 class Vehicle(ABC):
     def __init__(self, model: str, colour: str) -> None:
+        """Construct a vehicle object.
+
+        Args:
+            model (str): model of the vehicle
+            colour (str): colour of the vehicle
+        """
         self.__model = model
         self.__colour = colour
         self.__speed = 0
@@ -18,49 +24,105 @@ class Vehicle(ABC):
         self.__weight_values = (1, 2, 6, 2, 1)
 
     def set_position(self, new_position: float) -> None:
+        """Set the position of the vehicle.
+
+        Args:
+            new_position (float): new position for the vehicle
+        """
         self.__position = new_position
 
-    def set_speed(self, speed: float) -> float:
+    def set_speed(self, speed: float) -> None:
+        """Set the speed of the vehicle.
+
+        Args:
+            speed (float): the speed to be set
+        """
         self.__speed = speed
 
     def get_model(self) -> str:
+        """Get the model of the vehicle.
+
+        Returns:
+            str: model of the vehicle
+        """
         return self.__model
 
     def get_colour(self) -> str:
+        """Get the colour of the vehicle.
+
+        Returns:
+            str: colour of the vehicle
+        """
         return self.__colour
 
     def get_speed(self) -> float:
+        """Get the speed of the vehicle.
+
+        Returns:
+            float: speed of the vehicle
+        """
         return self.__speed
 
     def get_position(self) -> float:
+        """Get the position of the vehicle.
+
+        Returns:
+            float: position of the vehicle
+        """
         return self.__position
 
     def get_position_int(self) -> int:
+        """Get an integer position of the vehicle.
+
+        Returns:
+            int: integer position of the vehicle
+        """
         return int(self.__position)
 
     def move(self) -> None:
+        """Move the vehicle based on speed and position.
+        """
         new_position = self.get_position() + self.get_speed()
         self.set_position(new_position)
 
     def accelerate(self) -> None:
+        """Accelerate the vehicle based on speed and a random number.
+        """
         variance = random.choices(
             self.__acceleration_change, weights=self.__weight_values
         )
         variance = variance[0]
         self.set_speed(self.get_speed() + variance)
 
-    def get_accel(self) -> float:
-        return self.__accel
-
     @abstractmethod
     def get_icon(self) -> None:
+        """Abstract method to get the vehicle icon.
+        """
         pass
 
-    def __str__(self) -> str:
+    def __repr__(self) -> str:
+        """Return a representation of the vehicle.
+
+        Returns:
+            str: representation of the vehicle
+        """
         return f"Model: {self.get_model()}, Colour: {self.get_colour()}, Speed: {self.get_speed():.2f}, Position: {self.get_position():.2f}"
 
-    def __eq__(self, other: "Vehicle"):
-        return self.get_position() == other.get_position()
+    def __lt__(self, other: "Vehicle") -> bool:
+        """Determine whether a vehicle is less than another based on position
 
-    def __lt__(self, other: "Vehicle"):
+        Args:
+            other (Vehicle): another vehicle
+
+        Returns:
+            bool: True if self is behind other
+        """
         return self.get_position() < other.get_position()
+    
+    def __str__(self) -> str:
+        """Return a nicely formatted string about the vehicle
+
+        Returns:
+            str: a nicely formatted string about the vehicle
+        """
+        return self.get_colour() + " " + self.get_model()
